6eb4ec21686bf4fed23d1fd3be19cc77f0ebb8b2
MayurMansukh <Mansukhmayur99@gmail.com>
2021-02-23 11:44:42 +0530

    strt game at position 0

---
+++ SnakesAndLadders.java
@@ -0,0 +1,20 @@
+public class SnakesAndLadders
+{
+    void snakeGame() throws IOException
+    {
+      
+        System.out.println("WELCOME TO SNAKES AND LADDERS");
+        System.out.println("===================================================================================");
+        int player1Index=0;
+        System.out.println(" your current position is "+player1Index+".");
+     }
+public static void main(String[]args)
+    {
+        SnakesAndLadders s=new SnakesAndLadders();
+        s.snakeGame();
+    }
+}
+
+
+
+
