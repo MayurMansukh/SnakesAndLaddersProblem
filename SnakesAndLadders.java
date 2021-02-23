45f8b3a8a1887b68914bb53aa2a1cb4f44dc3194
MayurMansukh <Mansukhmayur99@gmail.com>
2021-02-23 13:03:41 +0530

    roll the dice

--- SnakesAndLadders.java
+++ SnakesAndLadders.java
@@ -1,19 +1,25 @@
 public class SnakesAndLadders
 {
-    void snakeGame() throws IOException
+    void snakeGame() 
     {
-      
+       
         System.out.println("WELCOME TO SNAKES AND LADDERS");
         System.out.println("===================================================================================");
+        System.out.println("Press an key to start the game...");
         int player1Index=0;
-        System.out.println(" your current position is "+player1Index+".");
+        System.out.println("your current position is "+player1Index+".");
+        
+	int dThrow = (int)Math.floor((Math.random()*6)+1);
+        System.out.println("The number on the die is "+dThrow);
+
      }
-public static void main(String[]args)
+public static void main(String[]args) 
     {
         SnakesAndLadders s=new SnakesAndLadders();
         s.snakeGame();
     }
 }
+
 
 
 
