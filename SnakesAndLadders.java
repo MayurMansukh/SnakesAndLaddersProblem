d3cf7fd3654138d00de81b1e7a4396437120cc08
MayurMansukh <Mansukhmayur99@gmail.com>
2021-02-23 13:20:23 +0530

    play with two player or when reach ladder then play again

--- SnakesAndLadders.java
+++ SnakesAndLadders.java
@@ -1,11 +1,12 @@
-
 public class SnakesAndLadders {
-    public static void snakeGame() {
-        System.out.println("WELCOME TO SNAKES AND LADDERS");
+    public static void snakeGame()  {
         System.out.println("===================================================================================");
-        int player1Index = 0;
-        int turn1 = 1;
-        while (player1Index != 100) {
+        int player1Index = 0, player2Index = 0;
+        int turn1 = 1, turn2 = 1;
+        while((player1Index!=100)||(player2Index!=100))
+        {
+            System.out.println();
+            System.out.println("Player1 turn");
             int dThrow = (int)Math.floor((Math.random()*6)+1);
             System.out.println("The number on the die is " + dThrow);
 
@@ -13,17 +14,27 @@ public class SnakesAndLadders {
             {
                 System.out.println("You will have to wait for the next turn, you have exceeded 100.");
             }
-            else{
-                player1Index += dThrow;
-                System.out.println(" your new position is "+player1Index+".");
-                  switch (player1Index) {
+            else if((player1Index+dThrow)==100)
+            {
+                System.out.println("Congratulations player 1, you have won the game! Better luck next time, Player 2.");
+                break;
+            }
+            else
+            {
+                player1Index+=dThrow;
+                System.out.println("your new position is "+player1Index+".");
+                switch (player1Index) {
                     case 4:
-                        System.out.println("Awright! You got the ladder on square 4! Your new position is 14. Your nearest ladder is at 16,");
-                        player1Index = 14;
+                        System.out.println("Awright! You got the ladder on square 4! you play again");
+                        dThrow = (int)Math.floor((Math.random()*6)+1);
+                        player1Index+=dThrow;
+                        System.out.println(" number of dice is "+dThrow+" your new position is "+player1Index+".");
                         break;
                     case 16:
-                        System.out.println("Awright! You got the ladder on square 16! Your new position is 82. That's a big jump!");
-                        player1Index = 82;
+                        System.out.println("Awright! You got the ladder on square 16! you play again");
+                        dThrow = (int)Math.floor((Math.random()*6)+1);
+                        player1Index+=dThrow;
+                        System.out.println(" number of dice is "+dThrow+" your new position is "+player1Index+".");
                         break;
                     case 17:
                         System.out.println("Whoops! You were caught by the snake at square 17. You're now down to square 8. :(");
@@ -37,27 +48,64 @@ public class SnakesAndLadders {
                     default:
                         System.out.println("No snakes or ladders here.");
                 }
-                        turn1++;
-            }
-                if(player1Index == 100)
+                System.out.println();
+                System.out.println("Player2 turn");
+                turn1++;
+                dThrow = (int)Math.floor((Math.random()*6)+1);
+                System.out.println("The number on the die is " + dThrow);
+
+                if((player2Index+dThrow)>100)
                 {
-                         System.out.println("\nno of dice play to win the game is " +turn1);
+                    System.out.println("You will have to wait for the next turn, you have exceeded 100.");
                 }
-                        dThrow++;
+                else if((player2Index+dThrow)==100)
+                {
+                    System.out.println("Congratulations player 2, you have won the game! Better luck next time, Player 1.");
+                    break;
+                }
+                else
+                {
+                    player2Index+=dThrow;
+                    System.out.println("your new position is "+player1Index+".");
+                    switch (player2Index) {
+                        case 4:
+                            System.out.println("Awright! You got the ladder on square 4! you play again");
+                            dThrow = (int)Math.floor((Math.random()*6)+1);
+                            player2Index+=dThrow;
+                            System.out.println(" number of dice is "+dThrow+" your new position is "+player2Index+".");
+                            break;
+                        case 16:
+                            System.out.println("Awright! You got the ladder on square 16! you play again");
+                            dThrow = (int)Math.floor((Math.random()*6)+1);
+                            player2Index+=dThrow;
+                            System.out.println(" number of dice is "+dThrow+" your new position is "+player2Index+".");
+                            break;
+                        case 17:
+                            System.out.println("Whoops! You were caught by the snake at square 17. You're now down to square 8. :(");
+                            player1Index = 8;
+                            break;
+                        case 54:
+                            System.out.println("Whoops! You were caught by the snake at square 54! You're now down to square 28. :(");
+                            player1Index = 28;
+                            break;
+
+                        default:
+                            System.out.println("No snakes or ladders here.");
+                    }
+                }
+
+            }
+            turn1++;
+            turn2++;
         }
 
     }
-
-    public static void main(String[] args)  {
-        SnakesAndLadders s = new SnakesAndLadders();
+    public static void main(String[]args)
+    {
+        SnakesAndLadders s=new SnakesAndLadders();
         s.snakeGame();
-
     }
 }
-
-
-
-
 
 
 
